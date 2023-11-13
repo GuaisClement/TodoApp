66c0175d3fab5f01b53b7f48ff0343a0a8987a53
ange.fremy <ange.fremy@reseau.eseo.fr>
2023-11-13 11:57:23 +0100

    Ajout du calendrier

--- src/header/Taskbar.tsx
+++ src/header/Taskbar.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { FaHome, FaList, FaCheck, FaCalendar} from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaHome, FaList, FaCheck, FaCalendar } from 'react-icons/fa';
 import './Taskbar.css';
+import Calendar from '../calendar/calendar'; // Assurez-vous d'importer le composant Calendar depuis le bon emplacement
 
 const Taskbar = () => {
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const toggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
   return (
     <div className="taskbar">
-        <div className="taskbar-icon"><FaHome /></div>
-        <div className="taskbar-icon"><FaList /></div>
-        <div className="taskbar-icon"><FaCheck /></div>
-        <div className="taskbar-icon"><FaCalendar /></div>
+      <div className="taskbar-icon"><FaHome /></div>
+      <div className="taskbar-icon"><FaList /></div>
+      <div className="taskbar-icon"><FaCheck /></div>
+      <div className="taskbar-icon" onClick={toggleCalendar}><FaCalendar /></div>
+
+      {showCalendar && <Calendar />}
     </div>
   );
 }
