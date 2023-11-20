48284aebd3cd6cb6fdd54a78a9de2adc35064924
GuaisClement <clement.guais@reseau.eseo.fr>
2023-11-20 10:37:40 +0100

    add addTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import './App.css'
-import Task from './task/Task'
+import Task from './list/task/Task'
 import Taskbar from './header/Taskbar'
+import { TaskModel } from './model/task-model';
+import { useState } from 'react';
+import AddTask from './list/add-task/add-task';
 
 function App() {
-  const taskProps = {
-    id: 1,
-    checked: false,
-    title: 'Faire les courses',
-    content: 'Acheter du lait, des œufs, et du pain.',
-    date: new Date(),
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      checked: false,
+      title: 'Faire les courses',
+      content: 'Acheter du lait, des œufs, et du pain.',
+      date: new Date(),
+    },
+    {
+      id: 2,
+      checked: true,
+      title: 'Faire les courses',
+      content: 'Acheter du lait, des œufs, et du pain.',
+      date: new Date(),
+    },
+    {
+      id: 3,
+      checked: false,
+      title: 'Faire les courses',
+      content: 'Acheter du lait, des œufs, et du pain.',
+      date: new Date(),
+    }
+  ]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddTask = (newTask: TaskModel) => {
+    // MAJ locale
+    setTasks([...tasks, newTask]);
   };
 
   return (
     <>
       <div className="App">
         <h1>Mon Application</h1>
-        {/* Autres composants de votre application */}
         <Taskbar />
-      </div>
-      <div className="card">
-        <Task {...taskProps}></Task>
+        <AddTask onAddTask={handleAddTask} />
+        
+        {tasks.map((tasks) => (
+          <div className="card" key={tasks.id}>
+            <Task {...tasks}></Task>
+          </div>
+        ))}
+        
       </div>
     </>
   )
---
+++ src/model/task-model.ts
@@ -0,0 +1,7 @@
+export type TaskModel = {
+    id: number,
+    checked: boolean
+    title: string,
+    content: string,
+    date: Date,
+}
