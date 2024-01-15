b15cc001ecaed36ba2bfc3a4ccd35c565d005e76
Guilbaud Maxime <maxime.guilbaud@reseau.eseo.fr>
2024-01-15 10:34:49 +0100

    add : dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Taskbar from './header/Taskbar';
 // import 'react-calendar/dist/Calendar.css';
 import MyCalendar from './calendar/calendar';
 import TaskList from './task-list/TaskList';
 import Home from './home/Home';
+import { CiDark, CiLight } from "react-icons/ci";
 
 function App() {
   const [activeView, setActiveView] = useState('home');
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+      setIsDarkMode(!isDarkMode);
+  };
 
   const switchView = (view: React.SetStateAction<string>) => {
     setActiveView(view);
   };
 
+  useEffect(() => {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color', isDarkMode ? '#242424' : '#ffffff');
+    root.style.setProperty('--bg-color-oposite', isDarkMode ? '#ffffff' : '#242424');
+}, [isDarkMode]);
+
   return (
-    <div className="App">
-      <Taskbar switchView={switchView} />
+    <div className={`App ${isDarkMode ? 'Component-dark-mode' : 'Component-light-mode'}`}>      
+      <Taskbar isDarkMode={isDarkMode} switchView={switchView} />
+      <div className="toggleDarkMode" onClick={toggleDarkMode}>
+        {isDarkMode ? <CiLight /> : <CiDark />}
+      </div>
+
       <main>
         {activeView === 'home' && <Home></Home>}
         {activeView === 'list' && <TaskList></TaskList>}
-        {activeView === 'check' && <p>Contenu de la vue Check</p>}
-        {activeView === 'calendar' && <p><MyCalendar/></p>}
+        {activeView === 'check' && <p>Contenu Archivé</p>}
+        {activeView === 'calendar' && <p><MyCalendar /></p>}
       </main>
     </div>
   );
--- src/header/Taskbar.tsx
+++ src/header/Taskbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { FaHome, FaList, FaCheck, FaCalendar } from 'react-icons/fa';
+import { FaHome, FaList, FaCalendar, FaQuestion } from 'react-icons/fa';
 import './Taskbar.css';
 
 interface TaskbarProps {
   switchView: (view: string) => void;
+  isDarkMode: boolean;
 }
 
-const Taskbar: React.FC<TaskbarProps> = ({ switchView }) => {
+const Taskbar: React.FC<TaskbarProps> = ({ switchView, isDarkMode}) => {
   return (
-    <div className="taskbar">
+    <div className={`taskbar ${isDarkMode ? 'Component-dark-mode' : 'Component-light-mode'}`}>
       <div className="taskbar-icon" onClick={() => switchView('home')}><FaHome /></div>
       <div className="taskbar-icon" onClick={() => switchView('list')}><FaList /></div>
-      <div className="taskbar-icon" onClick={() => switchView('check')}><FaCheck /></div>
-      <div className="taskbar-icon" onClick={() => switchView('calendar')}><FaCalendar /></div>
+      <div className="taskbar-icon" onClick={() => switchView('check')}><FaQuestion/></div>
+      <div className="taskbar-icon" onClick={() => switchView('calendar')}><FaCalendar /></div>      
     </div>
   );
 }
